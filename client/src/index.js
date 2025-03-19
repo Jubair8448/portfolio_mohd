61f5b7156759f870801ce27a9a38906ce05edaa5
Jubair8448 <internationalcorrbee@gmail.com>
2025-03-19 10:28:15 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "@material-tailwind/react";
+
 import { store } from "./app/store";
 import App from "./App";
 
@@ -11,10 +11,10 @@ const root = createRoot(container);
 
 root.render(
   <BrowserRouter>
-    <ThemeProvider>
+   
       <Provider store={store}>
         <App />
       </Provider>
-    </ThemeProvider>
+    
   </BrowserRouter>
 );
