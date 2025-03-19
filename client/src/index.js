0d97089c4dd4c3bd225f0544b92de54b310fcbde
Jubair8448 <internationalcorrbee@gmail.com>
2025-03-19 08:33:45 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,22 +4,17 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import { ThemeProvider } from "@material-tailwind/react";
-// import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom'; // Import HashRouter
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <>
-    <ThemeProvider>
-      <Provider store={store}>
+  <ThemeProvider>
+    <Provider store={store}>
+      <HashRouter>
         <App />
-      </Provider>
-    </ThemeProvider>
-  </>
+      </HashRouter>
+    </Provider>
+  </ThemeProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
