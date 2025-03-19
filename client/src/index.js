dc2c67353032e5115de2dfe21cc7eb298f120d47
Jubair8448 <internationalcorrbee@gmail.com>
2025-03-19 19:35:47 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,27 +1,22 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-//import './App.css';
-
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
 import { ThemeProvider } from "@material-tailwind/react";
-// import reportWebVitals from './reportWebVitals';
+import { HashRouter } from "react-router-dom"; // ✅ Use HashRouter for Vercel Deployment
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <>
+  <React.StrictMode>
     <ThemeProvider>
       <Provider store={store}>
-        <App />
+        <HashRouter>  {/* ✅ Ensures Routing Works on Vercel */}
+          <App />
+        </HashRouter>
       </Provider>
     </ThemeProvider>
-  </>
+  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
